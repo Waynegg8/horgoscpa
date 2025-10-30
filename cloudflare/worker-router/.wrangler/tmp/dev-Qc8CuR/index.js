cd350fc4eeff043587fa30d9af4c9c699db7f4ad
Waynegg8 <810742@gmail.com>
2025-10-30 17:49:31 +0800

    1

--- cloudflare/worker-router/.wrangler/tmp/dev-Qc8CuR/index.js
+++ cloudflare/worker-router/.wrangler/tmp/dev-Qc8CuR/index.js
@@ -301,7 +301,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-tyxhaK/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-baIFTX/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -333,7 +333,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-tyxhaK/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-baIFTX/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
