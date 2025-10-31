034e544ba5098365e2033d65ba768634b7fa8619
Waynegg8 <810742@gmail.com>
2025-10-31 21:31:58 +0800

    修复：删除错误的bcrypt迁移文件，已直接用PBKDF2更新数据库密码

--- cloudflare/worker-router/migrations/2025-10-31T001800Z_fix_passwords.sql
+++
@@ -1,7 +0,0 @@
--- Fix all user passwords to 111111
--- bcrypt hash of '111111' with salt rounds 10
-
-UPDATE Users 
-SET password_hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
-WHERE username IN ('admin', 'liu', 'tian', 'chen');
-
